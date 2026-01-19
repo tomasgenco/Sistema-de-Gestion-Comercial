af624646828651e432da40b398758af6fcbd01f8
Tomas Genco <tomasgenco9@gmail.com>
2026-01-19 17:39:24 -0300

    cambios para cliente en PROD

--- src/main/java/com/ApiRestStock/CRUD/shared/security/SecurityConfig.java
+++ src/main/java/com/ApiRestStock/CRUD/shared/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.ApiRestStock.CRUD.shared.security;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -81,7 +80,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList(allowedOrigins.split("\\s*,\\s*")));
+        config.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:4173"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
