bca86b58191b0e562ba27887f75bbeec4d266cb4
Tomas Genco <tomasgenco9@gmail.com>
2026-01-20 12:48:52 -0300

    add url Front in cors-origin

--- src/main/java/com/ApiRestStock/CRUD/shared/security/CorsConfig.java
+++ src/main/java/com/ApiRestStock/CRUD/shared/security/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Value("${cors.allowed-origins:http://localhost:4173,http://localhost:5173}")
+    @Value("${cors.allowed-origins:http://localhost:4173,http://localhost:5173,https://sistema-de-gestion-comercial-front.onrender.com}")
     private String allowedOrigins;
 
     @Bean
